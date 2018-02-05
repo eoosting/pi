7cf09063db087a1467f3c7a7e7614b2dcf18c351
Eric Oosting <eoosting@users.noreply.github.com>
2018-02-04 21:15:51 -0500

    add pintest.py

---
+++ pintest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+while True:
+   GPIO.output(17, True)
+   time.sleep(3)
+   GPIO.output(17, False)
+   time.sleep(3)
+
