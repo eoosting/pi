d10254c2521cd8baf6c2801da93be8e8a91eb43a
Eric Oosting <eoosting@users.noreply.github.com>
2018-02-04 21:50:41 -0500

    add getget.sh

---
+++ getgit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ~pi/bin
+curl -O https://raw.githubusercontent.com/eoosting/pi/master/setupnotes.txt
+curl -O https://raw.githubusercontent.com/eoosting/pi/master/bluenotes.txt
+curl -O https://raw.githubusercontent.com/eoosting/pi/master/audionotes.txt
+curl -O https://raw.githubusercontent.com/eoosting/pi/master/ipannounce.py
+curl -O https://raw.githubusercontent.com/eoosting/pi/master/pibot.py
+curl -O https://raw.githubusercontent.com/eoosting/pi/master/mqttnotes.txt
+curl -O https://raw.githubusercontent.com/eoosting/pi/master/irnotes.txt
+curl -O https://raw.githubusercontent.com/eoosting/pi/master/gitnotes.txt
+curl -O https://raw.githubusercontent.com/eoosting/pi/master/getgit.sh
+chown pi:pi getgit.sh
+chmod ugo+rx getgit.sh
