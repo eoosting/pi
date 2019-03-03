1e672f80ac87ee5cc4f6687307da2994d5f56959
Eric Oosting <eoosting@users.noreply.github.com>
2019-03-02 21:04:54 -0500

    more help fixes

--- pibot.py
+++ pibot.py
@@ -81,11 +81,7 @@ if slack_client.rtm_connect():
                     slack_client.api_call(
                         "chat.postMessage",
                         channel=message['channel'],
-                        text="%s: usage:\n
-			    hostname: pring version, ram amd cpu for the names host\n
-			    cpu: have all listeners report on cpu\n
-			    mem: have all listeners report on memory\n
-			    ver: have all listeners report on pibot version and raspbian version\n" % (hostname),
+                        text="%s: usage:\n<hostname>: version, ram amd cpu for the names host\ncpu: have all listeners report on cpu\nmem: have all listeners report on memory\nver: have all listeners report on pibot version and raspbian version" % (hostname),
                         as_user=True)
 
                 if re.match(r'.*(memory|ram|mem).*', message_text, re.IGNORECASE):
